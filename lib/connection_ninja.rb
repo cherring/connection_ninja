b0cfeae78610bd888e5b14da77b390fb16761057
Chris Herring <chris.herring.iphone@gmail.com>
2009-06-07 15:48:43 +1000

    Remebered to take out my temp crap

--- lib/connection_ninja.rb
+++ lib/connection_ninja.rb
@@ -6,14 +6,14 @@ module ConnectionNinja
   
   module ClassMethods
     #These are just here to use to run my specs, will work out how to fake these later
-    RAILS_ENV = "development"
-     RAILS_ROOT = File.dirname(__FILE__) + "/.."
-     ActiveRecord::Base.configurations = { 'alternate_development' => { 
-       'adapter' => 'postgresql',
-       'database' => 'connection_ninja_alternate',
-       'host' => 'localhost'
-       }
-     }
+    # RAILS_ENV = "development"
+    #  RAILS_ROOT = File.dirname(__FILE__) + "/.."
+    #  ActiveRecord::Base.configurations = { 'alternate_development' => { 
+    #    'adapter' => 'postgresql',
+    #    'database' => 'connection_ninja_alternate',
+    #    'host' => 'localhost'
+    #    }
+    #  }
     
     def use_connection_ninja(config_name)
       connect_to_db(config(config_name))
